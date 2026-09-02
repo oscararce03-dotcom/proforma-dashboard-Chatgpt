75cdbc6fb685fde2a9cc9fceef1c0c5e4e458f94
oscararce03-dotcom <oscar.arce03@gmail.com>
2026-09-02 09:56:13 -0400

    Delete frontend directory

--- frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
-
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.GITHUB_ACTIONS === "true" && repo ? `/${repo}/` : "/",
-});
